c629996918ccdf373aa3fb33013af9f7c15f3a49
AxelMaksel <am3mail@gmail.com>
2022-10-13 21:41:48 +0700

    add Task

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("ВВеди число A ");
+int a = Convert.ToInt32( Console.ReadLine());
+Console.Write("ВВеди число B ");
+int b = Convert.ToInt32( Console.ReadLine());
+
+if (b*b == a)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("номер дня недели ");
+int a = Convert.ToInt32( Console.ReadLine());
+Console.Write("ВВеди число B ");
+int b = Convert.ToInt32( Console.ReadLine());
+
+if (b*b == a)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
