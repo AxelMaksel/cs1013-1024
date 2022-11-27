a0be8f376fff4a1478783b953545029724794e0a
AxelMaksel <am3mail@gmail.com>
2022-11-27 20:49:35 +0700

    Add task 64,66,68

---
+++ Task64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+int number = 0;
+while (number < 1)
+{
+    Console.WriteLine("Введите натуральное число или 0 для выхода");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number == 0) return;
+}
+Console.WriteLine($"Я выведу тебе числа от {number} до 1:");
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num}");
+    if (num != 1) Console.Write(", ");
+    NaturalNumbers(num - 1);
+
+}
+
+NaturalNumbers(number);
---
+++ Task65/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+Console.WriteLine("Введите 2 числа");
+Console.Write("Введите первое ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void NaturNum(int num1, int num2)
+{
+    if (num1 < num2)
+    {
+        Console.Write($"{num1} ");
+        NaturNum(num1 + 1, num2);
+    }
+
+    if (num1 > num2)
+    {
+        Console.Write($"{num1} ");
+        NaturNum(num1 - 1, num2);
+    }
+    if (num1 == num2) Console.Write($"{num1} ");
+
+}
+
+NaturNum(number1, number2);
